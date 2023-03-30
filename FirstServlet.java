b627fc35dbe0ed3b275ebb123c6902b5d061209c
Rahul Sharma <developerrahul671@gmail.com>
2023-03-30 10:38:15 +0530

    Add files via upload

--- FirstServlet.java
+++ FirstServlet.java
@@ -1,5 +1,31 @@
 package com.servlet;
 
-public class FirstServlet {
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class FirstServlet extends HttpServlet {
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+		
+		int k = i + j;
+		
+//		PrintWriter out = res.getWriter();
+//		out.println("sum is : "+k);
+		
+		req.setAttribute("k", k);
+		
+		RequestDispatcher rdDispatcher = req.getRequestDispatcher("sqr");
+		rdDispatcher.forward(req, res);
+
+		
+	}
 
 }
